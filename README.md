2ee2a1f86f1646a2121f53a56e1472a2cb1f6652
Alessio Vaccaro <alexvac@hotmail.it>
2018-11-06 18:52:55 +0100

    Added README and corrected some lines of code

---
+++ README.md
@@ -0,0 +1,24 @@
+# Simple Flask API Web Server
+### *From Plain text to JSON*
+
+### Info
+This is a simple project from my Data Science course made by graduate students.
+
+#### The Task
+Create an API Web Server by using the Flask micro framework to display data from a Plain Text file in JSON format.
+After receiving a request the server will respond with a JSON.
+
+#### Routes (Endpoints)
+These are the main 
+
+Index
+```https://127.0.0.1/```
+This shows the main page with an "Hello world!" text.
+
+Name
+```https://127.0.0.1/name?name=Alessio&surname=Vaccaro```
+This shows a page with an your name and surname.
+
+ReadFile
+```https://127.0.0.1/readFile```
+This shows a JSON obtained through the *"my_model.py"* and *"my_file.txt"* file and its functions.
--- my_server.py
+++ my_server.py
@@ -3,20 +3,23 @@ from my_model import *
 
 app = Flask(__name__)
 
+# Simple routing with Template
 @app.route("/")
 def index():
-    return "Hello World!"
+    return render_template('myIndexTemplate.html')
 
-@app.route("/hello")
-def hello():
-    nome = request.args.get("nome")
-    cognome = request.args.get("cognome")
-    #response = "I am %s %s" % (nome, cognome)
-    return render_template('myTemplate.html', nomeTemp=nome, cognomeTemp=cognome)
+# Simple routing with two arguments "name" and "surname" with Template
+@app.route("/name")
+def name():
+    name = request.args.get("name")
+    surname = request.args.get("surname")
+    return render_template('myTemplate.html', nameTemp=name, surnameTemp=surname)
 
+# API Routing to get JSON file
 @app.route("/readFile")
 def readFileRoute():
     result = readTxtToJSON()
     return str(result)
 
-app.run()
+if __name__ == "__main__":
+	app.run()
